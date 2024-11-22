bdeefaf5a761183177623500d41a01e51f972507
Cratermn <89054984+Cratermn@users.noreply.github.com>
2024-11-22 14:38:58 -0800

    Add files via upload

--- go_analysis.R
+++ go_analysis.R
@@ -3,14 +3,26 @@ suppressPackageStartupMessages(library(DESeq2))
 suppressPackageStartupMessages(library(pheatmap)) 
 suppressPackageStartupMessages(library(topGO))
 
+library(stringr) 
+
 #### Load GO mapping file and create character vector with all gene IDs from the file ####
 geneID2GO <- readMappings("go_mapping.tsv")
 geneUniverse <- names(geneID2GO)
 
+antibiotic1 = "ciprofloxacin"
+antibiotic2 = "moxifloxacin"
+antibiotic3 = "tobramycin"
+
+exp = "vs_ctrl_30"
+time = "30m"
+
 #### Load differential expression analysis data ####
-colistin_vs_control_90 <- read_csv("de_seq_results/colistin_vs_ctrl_90.csv")
-imipenem_vs_control_90 <- read_csv("de_seq_results/imipenem_vs_ctrl_90.csv")
-meropenem_vs_control_90 <- read_csv("de_seq_results/meropenem_vs_ctrl_90.csv")
+file1 = paste(paste(antibiotic1, exp, sep="_"), "csv", sep=".")
+file2 = paste(paste(antibiotic2, exp, sep="_"), "csv", sep=".")
+file3 = paste(paste(antibiotic3, exp, sep="_"), "csv", sep=".")
+colistin_vs_control_90 <- read_csv(file1)
+imipenem_vs_control_90 <- read_csv(file2)
+meropenem_vs_control_90 <- read_csv(file3)
 
 #### Find statistically significant up/down-regulation of genes ####
 # Upregulated genes
@@ -55,40 +67,44 @@ names(colistin_down_gene_list) <- geneUniverse
 names(imipenem_down_gene_list) <- geneUniverse
 names(meropenem_down_gene_list) <- geneUniverse
 
+abTime1 = paste(str_to_title(antibiotic1), time, sep="_")
+abTime2 = paste(str_to_title(antibiotic2), time, sep="_")
+abTime3 = paste(str_to_title(antibiotic3), time, sep="_")
+
 #### Build GOdata objects for up/down-regulated data ####
 colistin_up_GO_data <- new("topGOdata", 
-                  description = "Colisin_90min", 
+                  description = abTime1, 
                   ontology = "BP", 
                   allGenes = colistin_up_gene_list,
                   annot = annFUN.gene2GO,
                   gene2GO = geneID2GO)
 imipenem_up_GO_data <- new("topGOdata", 
-                           description = "Imipenem_90min", 
+                           description = abTime2, 
                            ontology = "BP", 
                            allGenes = imipenem_up_gene_list,
                            annot = annFUN.gene2GO,
                            gene2GO = geneID2GO)
 meropenem_up_GO_data <- new("topGOdata", 
-                           description = "Meropenem_90min", 
+                           description = abTime3, 
                            ontology = "BP", 
                            allGenes = meropenem_up_gene_list,
                            annot = annFUN.gene2GO,
                            gene2GO = geneID2GO)
 
 colistin_down_GO_data <- new("topGOdata",
-                    description = "Colisin_90min",
+                    description = abTime1,
                     ontology = "BP",
                     allGenes = colistin_down_gene_list,
                     annot = annFUN.gene2GO,
                     gene2GO = geneID2GO)
 imipenem_down_GO_data <- new("topGOdata",
-                             description = "Imipenem_90min",
+                             description = abTime2,
                              ontology = "BP",
                              allGenes = imipenem_down_gene_list,
                              annot = annFUN.gene2GO,
                              gene2GO = geneID2GO)
 meropenem_down_GO_data <- new("topGOdata",
-                             description = "Meropenem_90min",
+                             description = abTime3,
                              ontology = "BP",
                              allGenes = meropenem_down_gene_list,
                              annot = annFUN.gene2GO,
@@ -115,37 +131,45 @@ meropenem_down_result <- runTest(meropenem_down_GO_data,
                                 algorithm = "weight01",
                                 statistic = "fisher")
 
+upRes1 = paste(antibiotic1, "up_result", sep="_")
+upRes2 = paste(antibiotic2, "up_result", sep="_")
+upRes3 = paste(antibiotic3, "up_result", sep="_")
+
+downRes1 = paste(antibiotic1, "down_result", sep="_")
+downRes2 = paste(antibiotic2, "down_result", sep="_")
+downRes3 = paste(antibiotic3, "down_result", sep="_")
+
 #### Extract a summary of up/down-regulated gene results ####
 colistin_up_GO <- GenTable(colistin_up_GO_data,
                            weight01 = colistin_up_result,
-                           orderBy = "colistin_up_result",
-                           ranksOf = "colistin_up_result",
+                           orderBy = upRes1,
+                           ranksOf = upRes1,
                            topNodes = 50)
 imipenem_up_GO <- GenTable(imipenem_up_GO_data,
                            weight01 = imipenem_up_result,
-                           orderBy = "imipenem_up_result",
-                           ranksOf = "imipenem_up_result",
+                           orderBy = upRes2,
+                           ranksOf = upRes2,
                            topNodes = 50)
 meropenem_up_GO <- GenTable(meropenem_up_GO_data,
                            weight01 = meropenem_up_result,
-                           orderBy = "meropenem_up_result",
-                           ranksOf = "meropenem_up_result",
+                           orderBy = upRes3,
+                           ranksOf = upRes3,
                            topNodes = 50)
 
 colistin_down_GO <- GenTable(colistin_down_GO_data,
                              weight01 = colistin_down_result,
-                             orderBy = "colistin_down_result",
-                             ranksOf = "colistin_down_result",
+                             orderBy = downRes1,
+                             ranksOf = downRes1,
                              topNodes = 50)
 imipenem_down_GO <- GenTable(imipenem_down_GO_data,
                              weight01 = imipenem_down_result,
-                             orderBy = "imipenem_down_result",
-                             ranksOf = "imipenem_down_result",
+                             orderBy = downRes2,
+                             ranksOf = downRes2,
                              topNodes = 50)
 meropenem_down_GO <- GenTable(meropenem_down_GO_data,
                              weight01 = meropenem_down_result,
-                             orderBy = "meropenem_down_result",
-                             ranksOf = "meropenem_down_result",
+                             orderBy = downRes3,
+                             ranksOf = downRes1,
                              topNodes = 50)
 
 #### Filter out any non-significant data and calculate the gene ratio ####
@@ -281,18 +305,25 @@ gg_meropenem_down <- meropenem_down_GO_filtered_arranged %>%
   theme(panel.border = element_rect(color = "black"), panel.grid = element_line(colour = "grey96")) +
   scale_y_continuous(limits = c(0,1), breaks = seq(0, 1, 0.25), expand = c(0, 0)) # changes the scale of the axes
 
+outUp1 = paste(paste(upRes1, exp, sep="_"), "png", sep=".")
+outUp2 = paste(paste(upRes2, exp, sep="_"), "png", sep=".")
+outUp3 = paste(paste(upRes3, exp, sep="_"), "png", sep=".")
+
+outDown1 = paste(paste(downRes1, exp, sep="_"), "png", sep=".")
+outDown2 = paste(paste(downRes2, exp, sep="_"), "png", sep=".")
+outDown3 = paste(paste(downRes3, exp, sep="_"), "png", sep=".")
 
 #### Save visualizations to file ####
-ggsave(filename="figures/colistin_upregulated.png", plot = gg_colistin_up,
+ggsave(filename=outUp1, plot = gg_colistin_up,
        width = 15, height = 7)
-ggsave(filename="figures/imipenem_upregulated.png", plot = gg_imipenem_up,
+ggsave(filename=outUp2, plot = gg_imipenem_up,
        width = 15, height = 7)
-ggsave(filename="figures/meropenem_upregulated.png", plot = gg_meropenem_up,
+ggsave(filename=outUp3, plot = gg_meropenem_up,
        width = 15, height = 7)
 
-ggsave(filename="figures/colistin_downregulated.png", plot = gg_colistin_down,
+ggsave(filename=outDown1, plot = gg_colistin_down,
        width = 15, height = 7)
-ggsave(filename="figures/imipenem_downregulated.png", plot = gg_imipenem_down,
+ggsave(filename=outDown2, plot = gg_imipenem_down,
        width = 15, height = 7)
-ggsave(filename="figures/meropenem_downregulated.png", plot = gg_meropenem_down,
+ggsave(filename=outDown3, plot = gg_meropenem_down,
        width = 15, height = 7)
